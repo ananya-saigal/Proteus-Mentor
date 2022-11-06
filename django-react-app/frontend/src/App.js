ee6bbba6adf6d325825a4c3834fdcbe6f98f5462
azlannaeem <97074156+azlannaeem@users.noreply.github.com>
2022-11-06 03:58:45 -0500

    created more components

--- django-react-app/frontend/src/App.js
+++ django-react-app/frontend/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import Browse_subjects from './pages/Browse_subjects';
 import Resources from './components/Resources';
-import Browse_Subjects from './pages/Browse_subjects';
+import Browse_Subjects from './components/Browse_Subjects';
 import My_feed from './pages/My_feed';
 import Feed from './components/Feed'
 import { Menu } from "@mui/icons-material";
@@ -9,19 +9,10 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
     <div>
       <Menu />
       <Feed/>
-      {/* <My_feed /> */}
-      {/* <Browse_Subjects /> */}
-      {/* <Route path = "/My_feed" component={My_feed} />
-      <Route path = "/Browse_subjects" components = {Browse_subjects}/>
-      <Route path = "/Resources" components = {Resources}/> */}
-
-      {/* <Resources /> */}
     </div>
-    </BrowserRouter>
   );
 }
 
