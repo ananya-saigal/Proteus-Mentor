40aaa449fbf088fc3af9a7ab3e1f14a2ce1ebb65
azlannaeem <97074156+azlannaeem@users.noreply.github.com>
2022-11-06 08:56:36 -0500

    Update App.js

--- django-react-app/frontend/src/App.js
+++ django-react-app/frontend/src/App.js
@@ -14,47 +14,47 @@ import Navbar from './components/Navbar';
 
 function App() {
 
-//   const sliderClick= (slider) =>{
-//     alert();
-//   }
+  const sliderClick= (slider) =>{
+    alert();
+  }
 
-//   const slides = [{
-//     image: '../assets/1.jpeg',
-//     title: 'Linear Algebra',
-//     description: 'Learn about matrices and linear functions in 3D space!',
-//     clickEvent: sliderClick
-// },
-// {
-//     image: '../assets/2.jpeg',
-//     title: 'Calculus',
-//     description: 'Learn about proofs, integrals, and limits!',
-//     clickEvent: sliderClick
-// },
-// {
-//     image: '../assets/3.jpeg',
-//     title: 'Computer Science',
-//     description: 'Learn different languages such as Python, Java, and more!',
-//     clickEvent: sliderClick
-// },
-// {
-//     image: '../assets/4.png',
-//     title: 'Biology',
-//     description: 'Learn about the human body and different organisms!',
-//     clickEvent: sliderClick
-// },
-// {
-//     image: '../assets/5.png',
-//     title: 'Chemistry',
-//     description: 'Learn about the earth and its elements!',
-//     clickEvent: sliderClick
-// },
-// {
-//     image: '../assets/6.jpeg',
-//     title: 'Physics',
-//     description: 'Learn about general relative and space-time relativity!',
-//     clickEvent: sliderClick
-// }
-// ];
+  const slides = [{
+    image: '../assets/1.jpeg',
+    title: 'Linear Algebra',
+    description: 'Learn about matrices and linear functions in 3D space!',
+    clickEvent: sliderClick
+},
+{
+    image: '../assets/2.jpeg',
+    title: 'Calculus',
+    description: 'Learn about proofs, integrals, and limits!',
+    clickEvent: sliderClick
+},
+{
+    image: '../assets/3.jpeg',
+    title: 'Computer Science',
+    description: 'Learn different languages such as Python, Java, and more!',
+    clickEvent: sliderClick
+},
+{
+    image: '../assets/4.png',
+    title: 'Biology',
+    description: 'Learn about the human body and different organisms!',
+    clickEvent: sliderClick
+},
+{
+    image: '../assets/5.png',
+    title: 'Chemistry',
+    description: 'Learn about the earth and its elements!',
+    clickEvent: sliderClick
+},
+{
+    image: '../assets/6.jpeg',
+    title: 'Physics',
+    description: 'Learn about general relative and space-time relativity!',
+    clickEvent: sliderClick
+}
+];
 
   return (
     <>
